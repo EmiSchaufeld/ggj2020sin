c5fe3ddabdcf1cac84aac5b149b44d5fcdc9297c
emischaufeld <eschaufeld@gmail.com>
2020-02-01 10:48:01 -0500

    sprite changer
    we changin

---
+++ GGJ2020/Assets/Scripts/changeSprite.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeSprite : MonoBehaviour
+
+{
+    public Sprite []sprite;
+    public GameObject gameBoy;
+    int spriteNumber =0;
+    SpriteRenderer spriteRenderer;
+   
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Start(){
+        spriteRenderer.sprite= sprite[spriteNumber];
+    }
+    void Update()
+
+    {
+        SpriteRenderer spriteRenderer = gameBoy.GetComponent<SpriteRenderer>();
+        
+        spriteRenderer.sprite = sprite[spriteNumber];
+
+    }
+
+    public void SpriteChanger()
+    {
+        if(spriteNumber< sprite.Length){
+        spriteNumber++;
+        }
+       
+        if(spriteNumber == sprite.Length){
+            spriteNumber = 0;
+
+        }
+        
+
+         
+    }
+}
