2f1d24530c74d9600fdcf0bd1489884320ec0507
emischaufeld <eschaufeld@gmail.com>
2020-02-01 11:17:18 -0500

    hi becky
    yo

---
+++ GGJ2020/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class ChangeScene : MonoBehaviour
+{
+    public string sceneName;
+    // Start is called before the first frame update
+ 
+     public void ChangeScener()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ GGJ2020/Assets/Scripts/dontDestroy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class dontDestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+     private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+   
+}
